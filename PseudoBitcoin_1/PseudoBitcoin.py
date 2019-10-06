78602cc6a389302be4573e1e444140773e501344
shenperson <wayne19990609@gmail.com>
2019-10-07 03:10:02 +0800

    DB : read and write

--- PseudoBitcoin_1/PseudoBitcoin.py
+++ PseudoBitcoin_1/PseudoBitcoin.py
@@ -1,6 +1,15 @@
 import time
 import hashlib
+import json
+from collections import namedtuple
 
+class AdvancedJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, '__jsonencode__'):
+            return obj.__jsonencode__()
+        if isinstance(obj, set):
+            return list(obj)
+        return json.JSONEncoder.default(self, obj)
 
 class Block(object):
     def __init__(self, Height, transaction, PrevBlockHash=''):
@@ -15,20 +24,26 @@ class Block(object):
 
     def setHash(self):
         self.Hash = hashlib.sha256(
-            (self.data + str(self.Nonce)).encode('utf-8')).hexdigest()
+            (self.transaction+str(self.PrevBlockHash)+str(self.time) + str(self.Nonce)).encode('utf-8')).hexdigest()
 
     def mine(self):
         self.Hash = hashlib.sha256(
-            (self.data + str(self.Nonce)).encode('utf-8')).hexdigest()
+            (self.transaction+str(self.PrevBlockHash)+str(self.time) + str(self.Nonce)).encode('utf-8')).hexdigest()
 
         while int(self.Hash,16)>>(256-self.bits):
             self.Nonce += 1 
             self.Hash = hashlib.sha256(
-                (self.data + str(self.Nonce)).encode('utf-8')).hexdigest()
+                (self.transaction+str(self.PrevBlockHash)+str(self.time) + str(self.Nonce)).encode('utf-8')).hexdigest()
             print(self.Hash, '\r'*64, end='')
 
         print("block mined :", self.Hash)
 
+    # def toJSON(self):
+    #     # print(json.dumps(self, default=lambda o: o.__dict__, sort_keys=False, indent=4))
+    #     return json.dumps(self, default=lambda o: o.__dict__)
+    def __jsonencode__(self):
+        return json.dumps(self, default=lambda o: o.__dict__,indent=2)
+    
 
 class Blockchain(object):
     def __init__(self):
@@ -46,10 +61,23 @@ class Blockchain(object):
         self.chain.append(newBlock)
 
 
+    def toDB(self):
+        
+        with open ('./ShenCoin.json','w') as json_file:
+            json_file.write(json.dumps(self.chain,cls=AdvancedJSONEncoder,indent=2))
+
+    def readDB(self,path):
+        with open (path,'r') as json_file:
+            data = json.load(json_file)
+            for i in data:
+                self.chain.append(json.loads(i, object_hook=lambda d: namedtuple('X', d.keys())(*d.values())))
+                print(self.chain[-1].Hash)
+                
+
 def main():
+
     a = Blockchain()
-    a.addGenesisBlock('as')
-    a.addBlock('ass')
+    a.readDB('./ShenCoin.json')
 
 
 if __name__ == '__main__':
